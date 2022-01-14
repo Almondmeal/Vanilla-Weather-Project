941be75200bc493c037b78842632566d0e35dba9
christean <christeanng@gmail.com>
2022-01-15 08:52:57 +1100

    Edit relevant Icon to API

--- script.js
+++ script.js
@@ -23,18 +23,23 @@ let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
+let iconElement=document.querySelector("#icon");
+
 temperatureElement.innerHTML= Math.round(response.data.main.temp);
 cityElement.innerHTML=response.data.name;
-descriptionElement = response.data.weather[0].description;
+descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML= Math.round (response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt*1000);
+iconElement.setAttribute("src",  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
 }
 
 
-
+let city ="miami";
 let apiKey="a34cc778ea5bd61a888e8521250c7678";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
